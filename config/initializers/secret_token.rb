cea5af7c59f7a6be5ff64e6c79605c5e1e896510
Elise Worthy <eworthy@bluebox.net>
2011-10-11 08:39:21 -0700

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'db22e0cea37d952f18ef19eaab1c69d51e0c3b1a84178079feb901c118e98c601986882fbb79b1806fe9cd1d02ad0d3f0bf8275869d3fe7aca95b8134d781844'
